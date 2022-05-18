41e954a0324ca185e64d6e12713b4f734e55848e
coderamrin <coderamrin@gmail.com>
2022-05-18 16:07:11 +0600

    first edition done

---
+++ data.js
@@ -0,0 +1,43 @@
+const projectArr = [
+  {
+    title: 'CSS Projects',
+    description:
+      'An archive CSS projects. All projects are built with vanilla css and some javascript.',
+    live: 'https://build-10-css-projects.netlify.app/',
+    sourceCode: 'https://github.com/Coderamrin/build-10-css-projects',
+    image: './imgs/10-web-projects.png',
+  },
+  {
+    title: 'Library App',
+    description:
+      'A library app built with vanilla JavaScript and CSS. This app is a simple library app to store books you read and want to read.',
+    live: 'https://build-10-css-projects.netlify.app/',
+    sourceCode: 'https://coderamrin.github.io/library/',
+    image: './imgs/library.png',
+  },
+  {
+    title: 'Memory app',
+    description:
+      'This app is built with react. The main goal of this was to understand how state works and how to use it in react.',
+    live: 'https://coderamrin.github.io/memory_card/',
+    sourceCode: 'https://github.com/Coderamrin/memory_card',
+    image: './imgs/memory-game.png',
+  },
+  {
+    title: 'Mission Mars Game',
+    description:
+      'This is a game I built with my friends for the Odin Project Game Jam. This game is a simple game where you answer a few questions and send a ship to mars to win the game.',
+    live: 'https://swthurlow.github.io/mars-mission-gamejam/',
+    sourceCode: 'https://github.com/SWThurlow/mars-mission-gamejam',
+    image: './imgs/marse-game.png',
+  },
+];
+
+const blog = [
+  {
+    title: 'How to build a counter app with JavaScript',
+    readmore:
+      'https://coderamrin.hashnode.dev/how-to-build-a-counter-app-with-javascript',
+    image: '',
+  },
+];
---
+++ script.js
@@ -0,0 +1,38 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const navLinks = document.querySelector('.nav__links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+//project section
+const projctGrid = document.querySelector('.project__grid');
+
+//get projects from the data.js file
+const projects = projectArr;
+projects.map((project) => addProject(project));
+
+function addProject(project) {
+  const { title, description, live, sourceCode, image } = project;
+
+  console.log(image);
+
+  const projectItem = `
+    <div class="project__grid-item">
+        <img src=${image} alt="">
+        <h3>
+            ${title}
+        </h3>
+        <p>
+            ${description}
+        </p>
+        <div>
+        <a href=${live} target="_blank"> <button>  Live </button>
+        </a> 
+            <a href=${sourceCode} target="_blank" > <button> Source Code </button> 
+            </a> 
+        </div> 
+   </div>`;
+
+  projctGrid.insertAdjacentHTML('beforeend', projectItem);
+}
